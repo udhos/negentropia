1e4c797760609a0b458419ccd1e34eb8914e2853
Everton Marques <everton.marques@gmail.com>
2013-02-08 12:47:42 -0200

    Fix pathname.

--- win-run.cmd
+++ win-run.cmd
@@ -1,7 +1,7 @@
 @rem win-run
 
 start cmd /k c:\redisbin\redis-server.exe
-start cmd /k c:\tmp\devel\negentropia\win-run-webserver.cmd
+start cmd /k c:\tmp\devel\negentropia\win-run-webserv.cmd
 start cmd /k c:\tmp\devel\negentropia\win-run-world.cmd
 
 @rem eof
