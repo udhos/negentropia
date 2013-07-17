94b0775978c8e150ca99e9b6762e2f2cf7f14859
Everton Marques <everton.marques@gmail.com>
2013-07-17 01:34:56 -0300

    Run pub from command-line.

--- win-build.cmd
+++ win-build.cmd
@@ -4,7 +4,8 @@ set DEVEL=c:\tmp\devel
 set GOPATH=%DEVEL%\negentropia\webserv
 
 set DART_SDK=c:\dart\dart-sdk
-set NEG_DART_MAIN=%DEVEL%\negentropia\wwwroot\dart\negentropia_home.dart
+set NEG_DART_SRC=%DEVEL%\negentropia\wwwroot\dart
+set NEG_DART_MAIN=%NEG_DART_SRC%\negentropia_home.dart
 
 @rem install goauth2
 @rem
@@ -28,6 +29,13 @@ go install negentropia\webserv
 go install negentropia\world
 
 @rem build client
+set OLD_CD=%CD%
+cd %NEG_DART_SRC%
+call %DART_SDK%\bin\pub install
+@echo on
+call %DART_SDK%\bin\pub update
+@echo on
+cd %OLD_CD%
 %DART_SDK%\bin\dart2js -c -o %NEG_DART_MAIN%.js %NEG_DART_MAIN%
 
 @rem eof
