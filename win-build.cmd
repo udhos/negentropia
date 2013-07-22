05ce1e27eae80a0f87c2cc68cf9ffbb1c91ceeea
Everton Marques <everton.marques@gmail.com>
2013-07-22 18:23:28 -0300

    Call dartanalyzer from command-line.

--- win-build.cmd
+++ win-build.cmd
@@ -4,6 +4,9 @@ set DEVEL=c:\tmp\devel
 set GOPATH=%DEVEL%\negentropia\webserv
 
 set DART_SDK=c:\dart\dart-sdk
+
+@rem dartanalyzer munges DART_SDK
+set NEG_DART_SDK=%DART_SDK%
 set NEG_DART_SRC=%DEVEL%\negentropia\wwwroot\dart
 set NEG_DART_MAIN=%NEG_DART_SRC%\negentropia_home.dart
 
@@ -31,11 +34,13 @@ go install negentropia\world
 @rem build client
 set OLD_CD=%CD%
 cd %NEG_DART_SRC%
-call %DART_SDK%\bin\pub install
+call %NEG_DART_SDK%\bin\pub install
 @echo on
-call %DART_SDK%\bin\pub update
+call %NEG_DART_SDK%\bin\pub update
 @echo on
 cd %OLD_CD%
-%DART_SDK%\bin\dart2js -c -o %NEG_DART_MAIN%.js %NEG_DART_MAIN%
+call %NEG_DART_SDK%\bin\dartanalyzer %NEG_DART_MAIN%
+@echo on
+call %NEG_DART_SDK%\bin\dart2js -c -o %NEG_DART_MAIN%.js %NEG_DART_MAIN%
 
 @rem eof
