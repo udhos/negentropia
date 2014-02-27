a56ec240c07bfa7d01714c383339851da276c42e
Everton Marques <everton.marques@gmail.com>
2014-02-27 15:29:28 -0300

    Fetch non-persistent zone information.

--- webserv/src/negentropia/world/server/zone.go
+++ webserv/src/negentropia/world/server/zone.go
@@ -3,8 +3,37 @@ package server
 import (
 	"log"
 
+	"github.com/spate/vectormath"
+
 	"negentropia/webserv/store"
 )
+
+// Non-persitent instance data
+type Unit struct {
+	uid         string
+	front       vectormath.Vector3
+	up          vectormath.Vector3
+	linearSpeed float32 // m/s
+	yawSpeed    float32 // rad/s
+	pitchSpeed  float32 // rad/s
+}
+
+type Zone struct {
+	zid       string
+	unitTable map[string]*Unit
+}
+
+var (
+	zoneTable = map[string]*Zone{}
+)
+
+func newZone(zid string) *Zone {
+	return &Zone{zid: zid, unitTable: make(map[string]*Unit)}
+}
+
+func newUnit(uid string) *Unit {
+	return &Unit{uid: uid}
+}
 
 func updateAllZones() {
 	//
@@ -12,28 +41,50 @@ func updateAllZones() {
 	//
 	zones := store.QueryKeys("z:*")
 	if len(zones) < 1 {
-		log.Printf("server.updateAllZones: no zone found")
+		log.Printf("updateAllZones: no zone found")
 		return
 	}
 
-	for _, zone := range zones {
-		instanceList := store.QueryField(zone, "instanceList")
+	for _, zid := range zones {
+		zone, zok := zoneTable[zid]
+		if !zok {
+			zone = newZone(zid)
+			zoneTable[zid] = zone
+		}
+
+		if zone == nil {
+			log.Printf("updateAllZones: failure creating zone=%s", zid)
+			continue
+		}
+
+		instanceList := store.QueryField(zid, "instanceList")
 		if instanceList == "" {
-			log.Printf("server.updateAllZones: zone=%s: no instanceList", zone)
+			log.Printf("updateAllZones: zone=%s: no instanceList", zid)
 			continue
 		}
 
 		instances := store.QuerySet(instanceList)
 		if len(instances) < 1 {
-			log.Printf("server.updateAllZones: zone=%s: empty instanceList", zone)
+			log.Printf("updateAllZones: zone=%s: empty instanceList", zid)
 			continue
 		}
 
-		for _, inst := range instances {
-			coord := store.QueryField(inst, "coord")
-			mission := store.QueryField(inst, "mission")
+		for _, uid := range instances {
+			unit, uok := zone.unitTable[uid]
+			if !uok {
+				unit = newUnit(uid)
+				zone.unitTable[uid] = unit
+			}
 
-			log.Printf("server.updateAllZones: zone=%s instance=%s coord=%s mission=%s", zone, inst, coord, mission)
+			if unit == nil {
+				log.Printf("updateAllZones: failure creating unit=%s", zid)
+				continue
+			}
+
+			coord := store.QueryField(uid, "coord")
+			mission := store.QueryField(uid, "mission")
+
+			log.Printf("updateAllZones: zone=%s unit=%s coord=%s mission=%s", zid, uid, coord, mission)
 		}
 	}
 }
