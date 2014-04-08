41edbe83b3ca926bfb5b1f2e57bb4a0275caf6b5
Everton Marques <everton.marques@gmail.com>
2014-04-08 12:16:41 -0300

    Identify loopback address.

--- webserv/src/negentropia/samples/myip.go
+++ webserv/src/negentropia/samples/myip.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"os"
 )
@@ -18,20 +19,16 @@ func myip() {
 
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
-		os.Stderr.WriteString("error: " + err.Error() + "\n")
+		fmt.Errorf("error: %v\n", err.Error())
 		return
 	}
 
 	for _, a := range addrs {
-		if ipnet, ok := a.(*net.IPNet); ok {
-			os.Stdout.WriteString(ipnet.IP.String())
-			if ipnet.IP.IsLoopback() {
-				os.Stdout.WriteString(" loopback\n")
-			} else {
-				os.Stdout.WriteString(" non-loopback\n")
-			}
-		}
+		ip := net.ParseIP(a.String())
+		fmt.Printf("addr: %v loopback=%v\n", a, ip.IsLoopback())
 	}
+
+	fmt.Println()
 }
 
 func myip2() {
@@ -39,33 +36,27 @@ func myip2() {
 
 	tt, err := net.Interfaces()
 	if err != nil {
-		os.Stderr.WriteString("error: " + err.Error() + "\n")
+		fmt.Errorf("error: %v\n", err.Error())
 		return
 	}
 	for _, t := range tt {
 		aa, err := t.Addrs()
 		if err != nil {
-			os.Stderr.WriteString("error: " + err.Error() + "\n")
+			fmt.Errorf("error: %v\n", err.Error())
 			continue
 		}
 		for _, a := range aa {
-			ipnet, ok := a.(*net.IPNet)
-			if !ok {
-				continue
-			}
-			v4 := ipnet.IP.To4()
-			if v4 == nil || v4[0] == 127 { // loopback address
-				os.Stdout.WriteString(t.Name + " " + v4.String() + " loopback\n")
-			} else {
-				os.Stdout.WriteString(t.Name + " " + v4.String() + " non-loopback\n")
-			}
+			ip := net.ParseIP(a.String())
+			fmt.Printf("%v addr: %v loopback=%v\n", t.Name, a, ip.IsLoopback())
 		}
 	}
+
+	fmt.Println()
 }
 
 func main() {
-	os.Stdout.WriteString("myip -- begin\n")
+	fmt.Println("myip -- begin")
 	myip()
 	myip2()
-	os.Stdout.WriteString("myip -- end\n")
+	fmt.Println("myip -- end")
 }
