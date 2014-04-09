4b7226dd485256d2b1bdbc1f774f1c901409af16
Everton Marques <everton.marques@gmail.com>
2014-04-09 05:21:22 -0300

    Tests.

--- webserv/src/negentropia/world/server/vec_test.go
+++ webserv/src/negentropia/world/server/vec_test.go
@@ -8,21 +8,24 @@ import (
 
 func expectWant(t *testing.T, in, want string) {
 	var vec3 vectormath.Vector3
+	
+	t.Logf("expectWant: in=[%v] want=[%v]", in, want)
 
 	if err := parseVector3(&vec3, in); err != nil {
 		t.Errorf("parseVector3(%v): err=%v", in, err)
 		return
 	}
 
-	out := vector3String(vec3)
-	if out != want {
-		t.Errorf("parseVector3(%v) = %v, want %v", in, out, want)
+	if out := vector3String(vec3); out != want {
+		t.Errorf("parseVector3(%v)=[%v], want [%v]", in, out, want)
 	}
 }
 
 func expectErr(t *testing.T, in string) {
 	var vec3 vectormath.Vector3
 
+	t.Logf("expectErr: in=[%v]", in)
+	
 	if err := parseVector3(&vec3, in); err == nil {
 		t.Errorf("parseVector3(%v): accepted, but wanted error", in)
 	}
@@ -30,8 +33,18 @@ func expectErr(t *testing.T, in string) {
 
 func TestParseVector3(t *testing.T) {
 	expectWant(t, "1,2,3", "1,2,3")
+	expectWant(t, "1.1,2.2,3.3", "1.1,2.2,3.3")
 	expectWant(t, "1.0,2.0,3.0", "1,2,3")
 	expectWant(t, " 1.0 , 2.0 , 3.0 ", "1,2,3")
+	expectErr(t, "")
+	expectErr(t, " ")
+	expectErr(t, ",")
+	expectErr(t, " , ")
+	expectErr(t, ",,")
+	expectErr(t, " , , ")
+	expectErr(t, ",,,")
+	expectErr(t, " , , , ")
+	expectErr(t, "1,2,3,4")
 	expectErr(t, "1.0x,2.0,3.0")
 	expectErr(t, "1.0,2.0x,3.0")
 	expectErr(t, "1.0,2.0,3.0x")
--- win-gotest.cmd
+++ win-gotest.cmd
@@ -4,6 +4,6 @@ set DEVEL=c:\tmp\devel
 set GOPATH=%DEVEL%\negentropia\webserv
 
 @rem test
-go test negentropia\world\server
+go test -test.v negentropia\world\server
 
 @rem eof
