1659543e193be988f067871da42e7616f7635bc2
Everton Marques <everton.marques@gmail.com>
2014-04-10 17:35:04 -0300

    Example for running two webservers.

---
+++ win-run-webserv2.cmd
@@ -0,0 +1,6 @@
+@rem win-run-webserver
+
+set DEVEL=c:\tmp\devel
+%DEVEL%\negentropia\webserv\bin\webserv.exe -config=%DEVEL%\config-webserv2.txt -config=%DEVEL%\config-common.txt
+
+@rem eof
--- win-run.cmd
+++ win-run.cmd
@@ -1,7 +1,8 @@
 @rem win-run
 
 start cmd /k c:\redisbin\redis-server.exe
-start cmd /k c:\tmp\devel\negentropia\win-run-webserv.cmd
 start cmd /k c:\tmp\devel\negentropia\win-run-world.cmd
+start cmd /k c:\tmp\devel\negentropia\win-run-webserv.cmd
+start cmd /k c:\tmp\devel\negentropia\win-run-webserv2.cmd
 
 @rem eof
