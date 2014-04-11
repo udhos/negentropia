5ac1220f71c8b04cf1183034de78eed7a698d8bb
Everton Marques <everton.marques@gmail.com>
2014-04-11 09:42:22 -0300

    Clean-up.

--- webserv/src/negentropia/world/parser/parser.go
+++ webserv/src/negentropia/world/parser/parser.go
@@ -7,20 +7,22 @@ import (
 	"unicode"
 )
 
-func ParseFloatSliceFunc(text string, f func(rune) bool) ([]float64, error) {
-	list := strings.FieldsFunc(text, f)
-
+func parseFloatSlice(list []string) ([]float64, error) {
 	result := make([]float64, len(list))
 
 	for i, j := range list {
 		j = strings.TrimSpace(j)
 		var err error
 		if result[i], err = strconv.ParseFloat(j, 64); err != nil {
-			return nil, fmt.Errorf("parseFloatSlice: text=[%v] elem[%v]=[%s] failure: %v", text, i, j, err)
+			return nil, fmt.Errorf("parseFloatSlice: list=[%v] elem[%v]=[%s] failure: %v", list, i, j, err)
 		}
 	}
 
 	return result, nil
+}
+
+func ParseFloatSliceFunc(text string, f func(rune) bool) ([]float64, error) {
+	return parseFloatSlice(strings.FieldsFunc(text, f))
 }
 
 func ParseFloatSliceSpace(text string) ([]float64, error) {
@@ -30,20 +32,10 @@ func ParseFloatSliceSpace(text string) ([]float64, error) {
 func ParseFloatVectorFunc(text string, size int, f func(rune) bool) ([]float64, error) {
 	list := strings.FieldsFunc(text, f)
 	if s := len(list); s != size {
-		return nil, fmt.Errorf("parseFloatSlice: text=[%v] size=%v must be %v", text, s, size)
+		return nil, fmt.Errorf("ParseFloatVectorFunc: text=[%v] size=%v must be %v", text, s, size)
 	}
 
-	result := make([]float64, size)
-
-	for i, j := range list {
-		j = strings.TrimSpace(j)
-		var err error
-		if result[i], err = strconv.ParseFloat(j, 64); err != nil {
-			return nil, fmt.Errorf("parseFloatSlice: text=[%v] elem[%v]=[%s] failure: %v", text, i, j, err)
-		}
-	}
-
-	return result, nil
+	return parseFloatSlice(list)
 }
 
 func ParseFloatVectorSpace(text string, size int) ([]float64, error) {
