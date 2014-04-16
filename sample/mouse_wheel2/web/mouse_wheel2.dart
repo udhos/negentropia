a8aa8abbfbab0a7fd89f7cb0cb7fa9e6b62545d8
Everton Marques <everton.marques@gmail.com>
2014-04-16 05:05:42 -0300

    Mouse wheel unit.

---
+++ sample/mouse_wheel2/web/mouse_wheel2.dart
@@ -0,0 +1,28 @@
+import 'dart:html';
+
+DivElement _log = new DivElement();
+int _line = 0;
+
+void log(String msg) {
+  ++_line;
+  msg = "$_line $msg";
+  print(msg);
+  DivElement m = new DivElement();
+  m.text = msg;
+  _log.append(m);
+
+  List<Element> children = _log.children;
+  if (children.length > 20) {
+    children.removeAt(0);
+  }
+}
+
+void main() {
+  
+  document.body.append(_log);
+  
+  window.onMouseWheel.listen((WheelEvent e) {
+    e.preventDefault();
+    log("wheel: dx=${e.deltaX} dy=${e.deltaY}");
+  });
+}
