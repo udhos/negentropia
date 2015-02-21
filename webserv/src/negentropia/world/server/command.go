7ee3ff653f003f06a75b83ef251a1d5198368301
Everton Marques <everton.marques@gmail.com>
2015-02-20 22:31:02 -0200

    Switch zone.

--- webserv/src/negentropia/world/server/command.go
+++ webserv/src/negentropia/world/server/command.go
@@ -3,6 +3,7 @@ package server
 import (
 	"fmt"
 	"log"
+	"sort"
 	"strings"
 
 	"negentropia/webserv/store"
@@ -45,21 +46,56 @@ func missionNext(p *Player, unitId string) {
 	msgPlayer(p, fmt.Sprintf("unit %v: new mission: [%v]", unitId, unit.mission))
 }
 
+func playerToZone(p *Player, zid string) {
+	store.SetField(p.Email, "location", zid)
+	sendZone(p, zid)
+}
+
 func switchZone(p *Player) {
 
-	//
-	// 1. Scan zones
-	//
+	// 1. Get zone list
 	zones := store.QueryKeys("z:*") // FIXME: replace redis KEYS with redis SCAN
 	if len(zones) < 1 {
 		log.Printf("switchZone: no zone found")
 		return
 	}
 
-	log.Printf("switchZone: FIXME WRITEME")
+	// no zone!!
+	loc := store.QueryField(p.Email, "location")
+	if loc == "" {
+		playerToZone(p, zones[0]) // pick any zone
+		return
+	}
+
+	msgPlayer(p, fmt.Sprintf("switchZone: curr=%s", loc))
 
 	// 2. Sort zone list
 
+	sort.Strings(zones)
+
 	// 3. Switch to next zone
 
+	for i, zid := range zones {
+		if zid == loc {
+			// found current zone
+
+			var newZid string
+
+			if i < len(zones)-1 {
+				newZid = zones[i+1]
+			} else {
+				newZid = zones[0]
+			}
+
+			playerToZone(p, newZid)
+
+			msgPlayer(p, fmt.Sprintf("switchZone: old=%s new=%s", loc, newZid))
+
+			return
+		}
+	}
+
+	msg := fmt.Sprintf("switchZone: zone %s not found", loc)
+	log.Printf(msg)
+	msgPlayer(p, msg)
 }
