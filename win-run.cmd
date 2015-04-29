319275f9df3c27c9262f07114d5ab830ad03e4d3
Everton Marques <everton.marques@gmail.com>
2015-04-29 15:59:05 -0300

    Start redis server from devel location.

--- win-run.cmd
+++ win-run.cmd
@@ -1,8 +1,13 @@
 @rem win-run
 
+pushd c:\tmp\devel
+
 start cmd /k c:\redis\redis-server.exe
+
 start cmd /k c:\tmp\devel\negentropia\win-run-world.cmd
 start cmd /k c:\tmp\devel\negentropia\win-run-webserv.cmd
 start cmd /k c:\tmp\devel\negentropia\win-run-webserv2.cmd
+
+popd
 
 @rem eof
