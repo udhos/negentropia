5b216e334cfb5c048b012523cb8e153fae80f9fc
Everton Marques <everton.marques@gmail.com>
2015-05-15 17:31:49 -0300

    negoc: Clean-up.

--- webserv/src/negentropia/negoc/opengl.go
+++ webserv/src/negentropia/negoc/opengl.go
@@ -220,13 +220,12 @@ func setFrustumMatrix(perspectiveMatrix *Matrix4, left, right, bottom, top, near
 	r0c2 := float32((right + left) / right_minus_left)
 	r1c2 := float32((top + bottom) / top_minus_bottom)
 	r2c2 := float32(-(far + near) / far_minus_near)
-	r3c2 := float32(-1.0)
 	r2c3 := float32(-(two_near * far) / far_minus_near)
 
 	perspectiveMatrix.data = []float32{
 		r0c0, 0, 0, 0, // c0
 		0, r1c1, 0, 0, // c1
-		r0c2, r1c2, r2c2, r3c2, // c2
+		r0c2, r1c2, r2c2, -1, // c2
 		0, 0, r2c3, 0, // c3
 	}
 }
