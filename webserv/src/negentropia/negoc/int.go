ab19633c9904858f27cc215ebd177d23a6c1f86b
Everton Marques <everton.marques@gmail.com>
2015-07-21 10:58:17 -0300

    negoc: Normalize mouse wheel reading.

---
+++ webserv/src/negentropia/negoc/int.go
@@ -0,0 +1,8 @@
+package main
+
+func intAbs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
--- webserv/src/negentropia/negoc/mouse.go
+++ webserv/src/negentropia/negoc/mouse.go
@@ -15,30 +15,53 @@ func getCanvasCoord(canvas dom.Element, clientX, clientY int) (int, int) {
 	return canvasX, canvasY
 }
 
+func normalizeWheel(delta float64) int {
+	if int(delta*100)%5625 == 0 {
+		// IE11: Nx56.25
+		return int(delta*10000) / 5625
+	}
+	d := int(delta)
+	if intAbs(d) < 100 {
+		// Firefox: Nx3
+		return d * 100 / 3
+	}
+	if d%120 == 0 {
+		// IE: Nx120
+		return d * 100 / 120
+	}
+	if d%100 == 0 {
+		// Chrome, Opera: Nx100
+		return d
+	}
+	return d // unknown browser
+}
+
 func trapMouse(gameInfo *gameState) {
 
+	canvas := dom.WrapElement(gameInfo.canvas)
+
 	wheelHandler := func(ev dom.Event) {
-		wheelEv := ev.(*dom.WheelEvent)
-		log(fmt.Sprintf("wheelHandler: event=%v", wheelEv))
+		w := ev.(*dom.WheelEvent)
+		w.PreventDefault()
+		delta := normalizeWheel(w.DeltaY)
+		log(fmt.Sprintf("wheelHandler: deltaY=%v normalized=%v", w.DeltaY, delta))
 	}
 
 	mouseUpHandler := func(ev dom.Event) {
 		m := ev.(*dom.MouseEvent)
-		el := dom.WrapElement(gameInfo.canvas)
-		canvasX, canvasY := getCanvasCoord(el, m.ClientX, m.ClientY)
+		m.PreventDefault()
+		canvasX, canvasY := getCanvasCoord(canvas, m.ClientX, m.ClientY)
 		log(fmt.Sprintf("mouseUpHandler: %v,%v", canvasX, canvasY))
 	}
 
 	mouseDownHandler := func(ev dom.Event) {
 		m := ev.(*dom.MouseEvent)
-		el := dom.WrapElement(gameInfo.canvas)
-		canvasX, canvasY := getCanvasCoord(el, m.ClientX, m.ClientY)
+		m.PreventDefault()
+		canvasX, canvasY := getCanvasCoord(canvas, m.ClientX, m.ClientY)
 		log(fmt.Sprintf("mouseDownHandler: %v,%v", canvasX, canvasY))
 	}
 
-	el := dom.WrapElement(gameInfo.canvas)
-
-	el.AddEventListener("wheel", false, wheelHandler)
-	el.AddEventListener("mouseup", false, mouseUpHandler)
-	el.AddEventListener("mousedown", false, mouseDownHandler)
+	canvas.AddEventListener("wheel", false, wheelHandler)
+	canvas.AddEventListener("mouseup", false, mouseUpHandler)
+	canvas.AddEventListener("mousedown", false, mouseDownHandler)
 }
