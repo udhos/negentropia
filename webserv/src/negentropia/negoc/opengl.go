8e771f77faa601582c508ef75e46a90e95f3c971
Everton Marques <everton.marques@gmail.com>
2015-07-22 16:29:30 -0300

    negoc: Invert matrix for ray picking.

--- webserv/src/negentropia/negoc/opengl.go
+++ webserv/src/negentropia/negoc/opengl.go
@@ -19,7 +19,65 @@ func (m *Matrix4) copyFrom(src *Matrix4) {
 }
 
 func (m *Matrix4) invert() {
-	// Matrix4.invert() FIXME WRITEME
+	m.copyInverseFrom(m)
+}
+
+func (m *Matrix4) copyInverseFrom(src *Matrix4) error {
+	a00 := src.data[0]
+	a01 := src.data[1]
+	a02 := src.data[2]
+	a03 := src.data[3]
+	a10 := src.data[4]
+	a11 := src.data[5]
+	a12 := src.data[6]
+	a13 := src.data[7]
+	a20 := src.data[8]
+	a21 := src.data[9]
+	a22 := src.data[10]
+	a23 := src.data[11]
+	a30 := src.data[12]
+	a31 := src.data[13]
+	a32 := src.data[14]
+	a33 := src.data[15]
+
+	b00 := a00*a11 - a01*a10
+	b01 := a00*a12 - a02*a10
+	b02 := a00*a13 - a03*a10
+	b03 := a01*a12 - a02*a11
+	b04 := a01*a13 - a03*a11
+	b05 := a02*a13 - a03*a12
+	b06 := a20*a31 - a21*a30
+	b07 := a20*a32 - a22*a30
+	b08 := a20*a33 - a23*a30
+	b09 := a21*a32 - a22*a31
+	b10 := a21*a33 - a23*a31
+	b11 := a22*a33 - a23*a32
+
+	det := b00*b11 - b01*b10 + b02*b09 + b03*b08 - b04*b07 + b05*b06
+	if det == 0.0 {
+		m.copyFrom(src)
+		return errors.New("copyInverseFrom: null determinant")
+	}
+	invDet := 1.0 / det
+
+	m.data[0] = (a11*b11 - a12*b10 + a13*b09) * invDet
+	m.data[1] = (-a01*b11 + a02*b10 - a03*b09) * invDet
+	m.data[2] = (a31*b05 - a32*b04 + a33*b03) * invDet
+	m.data[3] = (-a21*b05 + a22*b04 - a23*b03) * invDet
+	m.data[4] = (-a10*b11 + a12*b08 - a13*b07) * invDet
+	m.data[5] = (a00*b11 - a02*b08 + a03*b07) * invDet
+	m.data[6] = (-a30*b05 + a32*b02 - a33*b01) * invDet
+	m.data[7] = (a20*b05 - a22*b02 + a23*b01) * invDet
+	m.data[8] = (a10*b10 - a11*b08 + a13*b06) * invDet
+	m.data[9] = (-a00*b10 + a01*b08 - a03*b06) * invDet
+	m.data[10] = (a30*b04 - a31*b02 + a33*b00) * invDet
+	m.data[11] = (-a20*b04 + a21*b02 - a23*b00) * invDet
+	m.data[12] = (-a10*b09 + a11*b07 - a12*b06) * invDet
+	m.data[13] = (a00*b09 - a01*b07 + a02*b06) * invDet
+	m.data[14] = (-a30*b03 + a31*b01 - a32*b00) * invDet
+	m.data[15] = (a20*b03 - a21*b01 + a22*b00) * invDet
+
+	return nil
 }
 
 // transform: multiply this matrix [m] by vector [x,y,z,w]
@@ -362,10 +420,9 @@ func unproject(camera *Matrix4, viewportX, viewportWidth, viewportY, viewportHei
 		return
 	}
 
-	var invertedCameraMatrix Matrix4
-	invertedCameraMatrix.copyFrom(camera)
-	invertedCameraMatrix.invert()
-	vx, vy, vz, vw := invertedCameraMatrix.transform(pX, pY, pZ, 1.0)
+	var invertedCamera Matrix4
+	invertedCamera.copyInverseFrom(camera)
+	vx, vy, vz, vw := invertedCamera.transform(pX, pY, pZ, 1.0)
 	if vw == 0.0 {
 		err = errors.New("unproject: unprojected pick point with W=0")
 		return
