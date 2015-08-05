9e4574e2cfc1a40f8033e9fe0fac677feb557bb7
Everton Marques <everton.marques@gmail.com>
2015-08-05 11:34:10 -0300

    negoc: Clean-up.

--- webserv/src/negentropia/negoc/picking.go
+++ webserv/src/negentropia/negoc/picking.go
@@ -4,19 +4,7 @@ import (
 	"fmt"
 )
 
-func pick(gameInfo *gameState, canvasX, canvasY int) {
-	var cameraMatrix Matrix4
-	cameraMatrix.copyFrom(&gameInfo.pMatrix) // camera = P
-
-	var V Matrix4
-	loadCameraViewMatrixInto(gameInfo, &gameInfo.cam, &V)
-
-	cameraMatrix.multiply(&V) // camera = P * V
-
-	nearX, nearY, nearZ, farX, farY, farZ, err := pickRay(&cameraMatrix, 0, gameInfo.viewportWidth, 0, gameInfo.viewportHeight, canvasX, canvasY)
-
-	log(fmt.Sprintf("pick: canvas=%v,%v near=%v,%v,%v far=%v,%v,%v error=%v", canvasX, canvasY, nearX, nearY, nearZ, farX, farY, farZ, err))
-
+func debugPick(gameInfo *gameState, cameraMatrix *Matrix4, nearX, nearY, nearZ, farX, farY, farZ float64) {
 	clipNearX, clipNearY, clipNearZ, clipNearW := cameraMatrix.transform(nearX, nearY, nearZ, 1)
 	ndcNearX, ndcNearY, ndcNearZ := clipNearX/clipNearW, clipNearY/clipNearW, clipNearZ/clipNearW
 	log(fmt.Sprintf("pick: projected ndcNear=%v,%v,%v", ndcNearX, ndcNearY, ndcNearZ))
@@ -30,6 +18,22 @@ func pick(gameInfo *gameState, canvasX, canvasY int) {
 
 	screenFarX, screenFarY, screenFarDepth := viewportTransform(0, gameInfo.viewportWidth, 0, gameInfo.viewportHeight, 0.0, 1.0, ndcFarX, ndcFarY, ndcFarZ)
 	log(fmt.Sprintf("pick: screenFar=%v,%v,%v", screenFarX, screenFarY, screenFarDepth))
+}
+
+func pick(gameInfo *gameState, canvasX, canvasY int) {
+	var cameraMatrix Matrix4
+	cameraMatrix.copyFrom(&gameInfo.pMatrix) // camera = P
+
+	var V Matrix4
+	loadCameraViewMatrixInto(gameInfo, &gameInfo.cam, &V)
+
+	cameraMatrix.multiply(&V) // camera = P * V
+
+	nearX, nearY, nearZ, farX, farY, farZ, err := pickRay(&cameraMatrix, 0, gameInfo.viewportWidth, 0, gameInfo.viewportHeight, canvasX, canvasY)
+
+	log(fmt.Sprintf("pick: canvas=%v,%v near=%v,%v,%v far=%v,%v,%v error=%v", canvasX, canvasY, nearX, nearY, nearZ, farX, farY, farZ, err))
+
+	debugPick(gameInfo, &cameraMatrix, nearX, nearY, nearZ, farX, farY, farZ)
 
 	ray := ray{nearX, nearY, nearZ, farX - nearX, farY - nearY, farZ - nearZ}
 
