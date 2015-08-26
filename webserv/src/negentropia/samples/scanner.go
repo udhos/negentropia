752111e1277268b5155fcdbdb2ab4fc64b130ed2
Everton Marques <everton.marques@gmail.com>
2015-08-26 14:32:37 -0300

    Extend sample scanner for reading from file.

--- webserv/src/negentropia/samples/scanner.go
+++ webserv/src/negentropia/samples/scanner.go
@@ -5,17 +5,49 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func main() {
-	log.Printf("reading from stdin")
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		fmt.Printf("line=[%v]\n", scanner.Text())
+
+	if len(os.Args) < 2 {
+		basename := filepath.Base(os.Args[0])
+		log.Fatalf("usage: %s input-files", basename)
 	}
-	log.Printf("reading from stdin - done")
+
+	for _, input := range os.Args[1:] {
+		readFrom(input)
+	}
+
+}
+
+func readFrom(filename string) {
+
+	var scanner *bufio.Scanner
+
+	if filename == "-" || filename == "--" {
+		log.Printf("reading from stdin")
+		scanner = bufio.NewScanner(os.Stdin)
+	} else {
+		log.Printf("readFrom: reading from: %s", filename)
+		input, err := os.Open(filename)
+		if err != nil {
+			log.Printf("readFrom: error: %v", err)
+			return
+		}
+		scanner = bufio.NewScanner(input)
+	}
+
+	lineNum := 0
+
+	for scanner.Scan() {
+		lineNum++
+		fmt.Printf("line %d: [%v]\n", lineNum, scanner.Text())
+	}
+
+	log.Printf("readFrom: done")
 
 	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		log.Printf("readFrom: error: %v", err)
 	}
 }
